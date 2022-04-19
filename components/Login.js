852e7dbbc5626f3ef3b705127fe914d2586703a0
Ali Cangül <97682611+Alicngl@users.noreply.github.com>
2022-04-19 15:08:26 +0300

    google degisti

--- components/Login.js
+++ components/Login.js
@@ -1,18 +1,28 @@
-import { Grid,Button } from "@mui/material";
-import GoogleIcon from "@mui/icons-material/Google"
-import { provider,auth } from "../firebase";
+import { Grid, Button } from "@mui/material";
+import GoogleIcon from "@mui/icons-material/Google";
+import { provider, auth } from "../firebase";
 import { signInWithPopup } from "firebase/auth";
 
-
-export default function Login(){
-
-    const loginGoogle=()=>{
-
-        signInWithPopup(auth,provider)
-    }
-    return(
-        <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" style={{minHeight:"100vh"}}>
-            <Button variant="contained" startIcon={<GoogleIcon/>} onClick={loginGoogle}>GOOGLE İLE GİRİŞ YAP</Button>
-        </Grid>
-    )
+export default function Login() {
+  const loginGoogle = () => {
+    signInWithPopup(auth, provider);
+  };
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Button
+        variant="contained"
+        startIcon={<GoogleIcon />}
+        onClick={loginGoogle}
+      >
+        Ali İLE GİRİŞ YAP
+      </Button>
+    </Grid>
+  );
 }
